8a2e8180458788d562d114405a0afac6f2a155ab
Mir__off__Shoh <98812035+pubgmobileplay@users.noreply.github.com>
2022-05-07 23:19:06 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,301 @@
+var item  = document.querySelector(".header__links")
+item.addEventListener("mouseover", function () {
+    item.style.color = "red"
+
+})
+item.style.color = "#000"
+item.addEventListener("mouseout", function () {
+    item.style.color = "#000"
+})
+
+var items  = document.querySelector(".header__links1")
+items.addEventListener("mouseover", function () {
+    items.style.color = "red"
+})
+items.style.color = "#000"
+items.addEventListener("mouseout", function () {
+    items.style.color = "#000"
+})
+var items1  = document.querySelector(".header__links2")
+items1.addEventListener("mouseover", function () {
+    items1.style.color = "red"
+})
+items1.style.color = "#000"
+items1.addEventListener("mouseout", function () {
+    items1.style.color = "#000"
+})
+
+
+var item1  = document.querySelector(".content__link")
+item1.addEventListener("mouseover", function () {
+    item1.style.background = "#FF6464"
+    item1.style.color = "#fff"
+})
+item1.style.color = "#000"
+item1.addEventListener("mouseout", function () {
+    item1.style.background = "#fff"
+    item1.style.color = "#000"
+})
+var item2  = document.querySelector(".header__nav")
+item2.style.background = "#EDF7FA"
+
+var item3  = document.querySelector(".section__text")
+item3.style.color = "#21243D"
+
+var item4  = document.querySelector(".section__text1")
+item4.style.color = "#21243D"
+item4.style.fontWeight = "700"
+item4.style.fontSize = "26px"
+item4.style.lineHeight = "38px"
+
+var item41  = document.querySelector(".section__text11")
+item41.style.color = "#21243D"
+item41.style.fontWeight = "700"
+item41.style.fontSize = "26px"
+item41.style.lineHeight = "38px"
+
+var item5  = document.querySelector(".section__text2")
+item5.style.color = "#21243D"
+item5.style.fontWeight = "400"
+item5.style.fontSize = "18px"
+item5.style.lineHeight = "26px"
+
+var item6  = document.querySelector(".content__text")
+item6.style.color = "#000"
+
+var item7  = document.querySelector(".section__text3")
+item7.style.color = "#21243D"
+item7.style.fontWeight = "400"
+item7.style.lineHeight = "24px"
+
+var item8  = document.querySelector(".section1__text")
+item8.style.color = "#21243D"
+item8.style.fontWeight = "500"
+item8.style.fontSize = "22px"
+item8.style.lineHeight = "60px"
+item8.style.marginBottom = "0"
+
+var item9  = document.querySelector(".section1__text1")
+item9.style.color = "#21243D"
+item9.style.fontWeight = "700"
+item9.style.fontSize = "30px"
+item9.style.lineHeight = "44px"
+
+var items2  = document.querySelector(".section1__text3")
+items2.style.color = "#8695A4"
+
+var items3  = document.querySelector(".section1__text2")
+items3.style.color = "#fff"
+items3.style.background = "#142850" 
+items3.style.fontWeight = "900" 
+items3.style.fontSize = "18px" 
+items3.style.lineHeight = "60px" 
+items3.style.borderRadius = "16px" 
+items3.style.width = "62px" 
+items3.style.height = "25px" 
+items3.style.alignItems = "center" 
+items3.style.display = "flex" 
+items3.style.justifyContent = "center"
+
+var items31  = document.querySelector(".section1__text5")
+items31.style.color = "#fff"
+items31.style.background = "#142850"
+items31.style.fontWeight = "900" 
+items31.style.fontSize = "18px" 
+items31.style.lineHeight = "60px" 
+items31.style.borderRadius = "16px" 
+items31.style.width = "62px" 
+items31.style.height = "25px" 
+items31.style.alignItems = "center" 
+items31.style.display = "flex" 
+items31.style.justifyContent = "center" 
+
+var items9  = document.querySelector(".section1__text6")
+items9.style.color = "#fff"
+items9.style.background = "#142850"
+items9.style.fontWeight = "900"
+items9.style.fontSize = "18px"
+items9.style.lineHeight = "60px"
+items9.style.borderRadius = "16px"
+items9.style.width = "62px"
+items9.style.height = "25px"
+items9.style.display = "flex"
+items9.style.justifyContent = "center"
+items9.style.alignItems = "center"
+
+var items3  = document.querySelector(".section1__text4")
+items3.style.color = "#21243D"
+
+var items4  = document.querySelector(".section__link")
+items4.style.color = "#00A8CC"
+items4.style.fontWeight = "400"
+items4.style.lineHeight = "24px"
+items4.style.textDecoration = "none"
+
+var items4  = document.querySelector(".section")
+items4.style.background = "#EDF7FA"
+items4.style.display = "flex"
+items4.style.alignItems = "center"
+items4.style.flexDirection = "column"
+items4.style.justifyContent = "center"
+items4.style.minHeight = "396px"
+
+
+var items5  = document.querySelector(".section__div2")
+items5.style.background = "#fff"
+items5.style.width = "418px"
+items5.style.padding = "20px"
+items5.style.boxShadow = "0px 4px 10px rgba(187, 225, 250, 0.25)"
+items5.style.borderRadius = "15px"
+
+var items6  = document.querySelector(".section__div21")
+items6.style.background = "#fff"
+items6.style.width = "418px"
+items6.style.padding = "20px"
+items6.style.boxShadow = "0px 4px 10px rgba(187, 225, 250, 0.25)"
+items6.style.borderRadius = "15px"
+
+var items7  = document.querySelector(".section1__text3")
+items7.style.color = "#8695A4"
+items7.style.fontWeight = "400"
+items7.style.fontWeight = "20px"
+items7.style.lineHeight = "29px"
+items7.style.marginBottom = "1.1799999999999993rem"
+
+var items8  = document.querySelector(".footer__div1")
+items8.style.display = "flex"
+items8.style.alignItems = "center"
+items8.style.justifyContent = "center"
+
+var items9  = document.querySelector(".footer__text")
+items9.style.fontWeight = "400"
+items9.style.fontSize = "14px"
+items9.style.lineHeight = "21px"
+items9.style.textAlign = "center"
+items9.style.color = "#21243D"
+items9.style.marginBottom = "0"
+items9.style.marginTop = "10px"
+
+var items11  = document.querySelector(".footer__div")
+items11.style.display = "flex"
+items11.style.alignItems = "center"
+items11.style.justifyContent = "center"
+items11.style.columnGap = "25px"
+items11.style.flexWrap = "wrap"
+
+var items12  = document.querySelector(".section1__text4")
+items12.style.fontWeight = "400"
+items12.style.maxWidth = "594px"
+items12.style.lineHeight = "24px"
+
+var items13  = document.querySelector(".header__form")
+items13.style.columnGap = "20px"
+items13.style.display = "flex"
+
+var items14  = document.querySelector(".header__links")
+items14.style.display = "flex"
+items14.style.alignItems = "center"
+items14.style.justifyContent = "center"
+items14.style.textDecoration = "none"
+items14.style.fontSize = "20px"
+items14.style.fontWeight = "600"
+items14.style.transition = ".5s"
+
+var items15  = document.querySelector(".header__links1")
+items15.style.display = "flex"
+items15.style.alignItems = "center"
+items15.style.justifyContent = "center"
+items15.style.textDecoration = "none"
+items15.style.fontSize = "20px"
+items15.style.fontWeight = "600"
+items15.style.transition = ".5s"
+
+var items16  = document.querySelector(".header__links2")
+items16.style.display = "flex"
+items16.style.alignItems = "center"
+items16.style.justifyContent = "center"
+items16.style.textDecoration = "none"
+items16.style.fontSize = "20px"
+items16.style.fontWeight = "600"
+items16.style.transition = ".5s"
+
+var items17  = document.querySelector(".navbar-brand")
+items17.style.display = "none"
+
+var items18  = document.querySelector(".content__text")
+items18.style.fontSize = "44px"
+items18.style.fontWeight = "700"
+items18.style.lineHeight = "60px"
+
+var items19  = document.querySelector(".header__content")
+items19.style.minHeight = "660px"
+items19.style.paddingTop = "50px"
+items19.style.display = "flex"
+items19.style.alignItems = "center"
+items19.style.justifyContent = "center"
+
+var items20  = document.querySelector(".content__div2")
+items20.style.display = "flex"
+items20.style.rowGap = "20px"
+items20.style.flexDirection = "column"
+
+var items21  = document.querySelector(".content__link")
+items21.style.fontWeight = "500"
+items21.style.fontSize = "20px"
+items21.style.lineHeight = "29px"
+items21.style.border = " #FF6464 2px solid"
+items21.style.textDecoration = "none"
+items21.style.paddingTop = "10px"
+items21.style.paddingBottom = "10px"
+items21.style.paddingRight = "10px"
+items21.style.paddingLeft = "10px"
+items21.style.transition = ".5s"
+
+var items22  = document.querySelector(".content__image")
+items22.style.width = "50%"
+
+var items23  = document.querySelector(".content__div3")
+items23.style.display = "flex"
+items23.style.alignItems = "center"
+items23.style.justifyContent = "center"
+
+var items24  = document.querySelector(".section__text")
+items24.style.fontWeight = "400"
+items24.style.fontSize = "22px"
+items24.style.lineHeight = "60px"
+items24.style.display = "flex"
+items24.style.alignItems = "flex-end"
+items24.style.marginBottom = "0"
+
+var items25  = document.querySelector(".header")
+items25.style.position = "fixed"
+items25.style.width = "100%"
+items25.style.minHeight = "50px"
+
+var items26  = document.querySelector(".section__div1")
+items26.style.display = "flex"
+items26.style.alignItems = "center"
+items26.style.justifyContent = "space-between"
+
+var items27  = document.querySelector(".header")
+items27.style.position = "fixed"
+items27.style.width = "100%"
+items27.style.minHeight = "50px"
+
+var items28  = document.querySelector(".section__div23")
+items28.style.display = "flex"
+items28.style.alignItems = "center"
+items28.style.columnGap = "25px"
+
+var items31  = document.querySelector(".section__div24")
+items31.style.display = "flex"
+items31.style.alignItems = "center"
+items31.style.columnGap = "25px"
+
+var items29  = document.querySelector(".section1")
+items29.style.minHeight = "811px"
+
+var items30  = document.querySelector(".section1__div2")
+items30.style.display = "flex"
+items30.style.alignItems = "center"
+items30.style.columnGap = "20px"
